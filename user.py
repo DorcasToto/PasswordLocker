98f1eb8338a5473df2083052a746c767720394c1
Moringa School <dorcasmosonik@gmail.com>
2020-10-10 09:17:46 +0300

    define functions

--- user.py
+++ user.py
@@ -1,2 +1,79 @@
 class User:
-    
+
+    def __init__(self):
+        pass
+
+    def createUser(self, parameter_list):
+        """
+        docstring
+        """
+        pass
+
+    def login(parameter_list):
+        """
+        docstring
+        """
+        pass
+
+   def saveUser(parameter_list):
+        """
+        docstring
+        """
+        pass
+
+    def displayUser(parameter_list):
+        """
+        docstring
+        """
+        pass
+
+    def deleteUser(parameter_list):
+        """
+        docstring
+        """
+        pass
+
+
+class Credentials:
+
+    def __init__(parameter_list):
+        """
+        docstring
+        """
+        pass
+
+    def saveCredential(parameter_list):
+        """
+        docstring
+        """
+        pass
+
+    def searchCredential(parameter_list):
+        """
+        docstring
+        """
+        pass
+
+    def displayCredential(parameter_list):
+        """
+        docstring
+        """
+        pass
+
+    def credentialExist(parameter_list):
+        """
+        docstring
+        """
+        pass
+
+    def loginWithCredential(parameter_list):
+        """
+        docstring
+        """
+        pass
+
+    def passwordGenerate(parameter_list):
+        """
+        docstring
+        """
+        pass
