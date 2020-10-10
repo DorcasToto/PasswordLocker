3ea040d25dcc25f2632fc7fc6ffcc97f4b4f15a7
Moringa School <dorcasmosonik@gmail.com>
2020-10-10 22:05:36 +0300

    user login

--- user.py
+++ user.py
@@ -7,19 +7,20 @@ class User:
         self.password = password
         self.isLoggedin = False
 
-    def createUser(self):
+    def createUser(user_name, password):
         """
         docstring
         """
         newUser = User(user_name, password)
         return newUser
 
-    def login(self, isLoggedIn):
+    def login(user_name, ):
         """
         docstring
         """
         if isLoggedIn == True:
             self.isLoggedIn = True
+            print("Logged in Successfully")
 
         else:
             print("Check your log in credentials")
@@ -126,9 +127,53 @@ class Credentials:
 
     if __name__ == "__main__":
 
-        print("Welcome to password Locker.An application that will help you manage your passwords and even generate new passwords")
+        print("Welcome to password Locker.An application that will help you manage your passwords and even generate new passwords.")
         while True:
-            print("use these short code to proceed:\n cc:Create new Account\n lg:Login")
+            print(
+                "Use these short code to proceed:\n\n 1. cc-Create new Account\n 2. lg-Login\n 3. ex-exit")
             shortCode = input().lower()
 
             if shortCode == 'cc':
+                print("New Account")
+                print("*"*50)
+                print("Username")
+                user_name = input()
+                print("Password")
+                password = input()
+
+                createUser = User.createUser(user_name, password)
+                User.saveUser(createUser)
+                print("\n")
+                print(
+                    f"Hi {user_name} Your Account has been created sucessfully")
+                break
+
+            elif shortCode == 'lg':
+                print("*"*100)
+                print("Enter your username and password")
+                print("*"*100)
+
+                print("Username")
+                user_name = input()
+                print("Password")
+                password = input()
+
+                for user in User.userList:
+                    if user_name == user.user_name:
+                        # User.login()
+                        print("Break")
+                break
+
+            elif shortCode == 'ex':
+
+                print("Bye. See you some other time.")
+            
+            else:
+                print("Invalid shortcode\n")
+
+        while True:
+            print("What would like to do?\n\n Use these shortcodes to proceed\n 1. nc - Create new Credential \n 2. ds - Display existing Credential\n 3. fc - Find a credential \n 4. dc = Delete an existing Credential \n 5. ex - Exit Application")
+
+            
+
+            break
